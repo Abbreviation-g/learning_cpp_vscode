7e2442d3991551f7bd69545ff0197b0ac046b08e
my <my@123hhh.com>
2026-08-12 10:50:30 +0800

    防止run task报错

--- cpp21/src/main.cpp
+++ cpp21/src/main.cpp
@@ -4,6 +4,7 @@
 int main() {
     std::cout << "Hello from cpp21 CMake project!" << std::endl;
     std::cout << "__cplusplus = " << __cplusplus << std::endl;
+    std::cout << "我是谁我在哪" << std::endl;
     system("pause");
     return 0;
 }
